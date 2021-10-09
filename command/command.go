bd37cb0ba13aabf43eaa796f9fcb9e50e24a03f8
Jeff Tang <yayaya6d16@gmail.com>
2021-10-09 22:18:18 +0800

    implement command for controll tv

--- command/command.go
+++ command/command.go
@@ -4,6 +4,30 @@ type SingleCommand interface {
 	Execute()
 }
 
+type PowerOnCommand struct {
+	tv Television
+}
+
+func (c *PowerOnCommand) Execute() {
+	c.tv.TurnOn()
+}
+
+type PowerOffCommand struct {
+	tv Television
+}
+
+func (c *PowerOffCommand) Execute() {
+	c.tv.TurnOff()
+}
+
 type NumCommand interface {
 	Execute(int)
+}
+
+type ChangeChannelCommand struct {
+	tv Television
+}
+
+func (c *ChangeChannelCommand) Execute(ch int) {
+	c.tv.ChangeChannel(ch)
 }
